734f8834c468c921f5eb6278d2dea75e4a300597
TizianoCausin01 <causintiziano@gmail.com>
2025-03-05 19:58:13 +0100

    first commit

---
+++ code/preprocessing/tProject1917_preproc2_cleaning.m
@@ -0,0 +1,92 @@
+function Project1917_preproc2_cleaning(parms)
+% Project 1917
+% preprocessing script 1 - bad channel and segment detection
+
+% loop over subjects
+for isub = parms.subjects
+
+    % set paths
+    rootdir = '\\cimec-storage5.unitn.it\MORWUR\Projects\INGMAR\Project1917';
+    datadir = sprintf('%s%sdata%ssub-%03d',rootdir,filesep,filesep,isub);
+    indir = sprintf('%s%spreprocessing',datadir,filesep);
+
+    % start up Fieldtrip
+    addpath('\\cimec-storage5.unitn.it\MORWUR\Projects\INGMAR\toolboxes\fieldtrip-20231220')
+    ft_defaults
+
+    % loop over runs
+    for irun = parms.runs
+
+        fn2load = sprintf('%s%sdata_reref_filt_trim_sub%03d_run%02d',indir,filesep,isub,irun);
+        load(fn2load,'data');
+
+        % filter for segment outliers in the frequency range we're interested
+        % in for now (i.e., max beta, or below 50)
+        cfg = [];
+        cfg.lpfilter = 'yes';
+        cfg.lpfreq = 50; % look at +low freq bc ur inter.d into slow fluctuations
+
+        data_filt = ft_preprocessing(cfg, data); % ft_preprocessing to apply filters
+
+        % segment continuous data into 500 msec 'fake trials'
+        cfg = [];
+        cfg.length               = .5;
+        data_segmented           = ft_redefinetrial(cfg, data_filt);
+        clear data_filt
+
+        cfg = [];
+        cfg.channel = {'MEG'};
+        cfg.method = 'summary';
+        cfg.layout = 'CTF275.lay';
+        cfg.keeptrial   = 'yes';
+        data_segmented = ft_rejectvisual(cfg, data_segmented); % automatic artifact rejection
+
+        % store which channels to keep
+        megchan_keep = data_segmented.label; % only the channels that were not rejected from rejectvisual (?)
+        fn2save = sprintf('%s%sbadchan_sub%03d_run%02d',indir,filesep,isub,irun);
+        save(fn2save, 'megchan_keep'); % outputs both channels and segments and then select bad channels
+
+        % extract bad segments from first test
+        BAD_lowfreq = data_segmented.cfg.artfctdef.summary.artifact-data_segmented.sampleinfo(1,1)+1;
+        clear data_segmented
+
+        % filter for muscle activity, cut into 500 msec segments, and run ft_rejecvisual summary again
+        cfg = [];
+        cfg.bpfilter = 'yes';
+        cfg.bpfreq = [110 140]; % keep these frequencies to detect muscle activity ->
+        cfg.bpfilttype = 'but'; % butterworth filter
+        cfg.bpfiltord = 4;
+        cfg.hilbert = 'yes';
+        cfg.channel = megchan_keep;
+
+        data_filt = ft_preprocessing(cfg, data); 
+
+        % segment continuous data into 500 msec 'fake trials'
+        cfg                      = [];
+        cfg.length               = .5;
+        data_segmented           = ft_redefinetrial(cfg, data_filt); % and we'll remove these small "trials" when they present heavy muscle artifacts w/ rejectvisual 
+        clear data_filt
+
+        cfg = [];
+        cfg.channel = {'MEG'};
+        cfg.method = 'summary';
+        cfg.layout = 'CTF275.lay';
+        cfg.keepchannel = 'yes';
+        cfg.keeptrial   = 'yes';
+        data_segmented = ft_rejectvisual(cfg, data_segmented); % again rejectvisual after the bandpass 110-140
+
+        % extract bad segments
+        BAD_muscle = data_segmented.cfg.artfctdef.summary.artifact-data_segmented.sampleinfo(1,1)+1;
+        clear data_segmented
+
+        % convert from indices to time to prevent mistakes later on when
+        % removing bad segments on downsampled data
+        BAD_lowfreq = data.time{1}(BAD_lowfreq);
+        BAD_muscle = data.time{1}(BAD_muscle);
+
+        fn2save = sprintf('%s%sbadseg_sub%03d_run%02d',indir,filesep,isub,irun);
+        save(fn2save,'BAD_lowfreq','BAD_muscle');
+
+    end% run loop
+
+end% subject loop
---
+++ code/preprocessing/tProject1917_preproc4_ICAcheck.m
@@ -0,0 +1,96 @@
+function Project1917_preproc4_ICAcheck(parms)
+% Project 1917
+% preprocessing - IC check and rejection
+
+% set paths
+rootdir = '\\cimec-storage5.unitn.it\MORWUR\Projects\INGMAR\Project1917';
+
+% start up Fieldtrip
+addpath('\\cimec-storage5.unitn.it\MORWUR\Projects\INGMAR\toolboxes\fieldtrip-20231220')
+ft_defaults
+
+for isub = parms.subjects
+
+    indir = sprintf('%s%sdata%ssub-%03d%spreprocessing',rootdir,filesep,filesep,isub,filesep);
+
+    for irun = parms.runs
+
+        fn2load = sprintf('%s%sdata_reref_filt_trim_sub%03d_run%02d',indir,filesep,isub,irun);
+        load(fn2load,'data');
+
+        % load bad channels and bad segments and remove before ICA checking
+        fn2load = sprintf('%s%sbadchan_sub%03d_run%02d',indir,filesep,isub,irun);
+        load(fn2load, 'megchan_keep');
+
+        cfg = [];
+        cfg.channel = megchan_keep(:)';
+        data = ft_selectdata(cfg, data);
+
+        fn2load = sprintf('%s%sbadseg_sub%03d_run%02d',indir,filesep,isub,irun);
+        load(fn2load, 'BAD_lowfreq','BAD_muscle');
+
+        % combine two sources of bad segments
+        badsegs = [BAD_lowfreq ; BAD_muscle];
+
+        for iseg = 1:size(badsegs,1)
+            badsegs(iseg,:) = dsearchn(data.time{1}',badsegs(iseg,:)')';
+        end
+
+        % create logical vector of bad segments
+        samples2keep = true(size(data.time{1}));
+        for iseg = 1:size(badsegs,1)
+            samples2keep(badsegs(iseg,1):badsegs(iseg,2)) = false;
+        end
+
+        % load ICA components
+        fn2load = sprintf('%s%sica_weights_sub%03d_run%02d',indir,filesep,isub,irun);
+        load(fn2load,'unmixing', 'topolabel');
+
+        % now plot components for check
+        cfg = [];
+        cfg.method = 'predefined mixing matrix';
+        cfg.demean = 'no';
+        cfg.channel = {'MEG'};
+        cfg.topolabel = topolabel;
+        cfg.unmixing = unmixing;
+        comp = ft_componentanalysis(cfg, data);
+
+        comp.trial{1}(:,~samples2keep) = NaN;
+
+        cfg = [];
+        cfg.viewmode = 'component';
+        cfg.layout = 'CTF275_helmet.mat';
+        cfg.blocksize = 15;
+        cfg.channel = comp.label(1:20);
+        cfg.compscale = 'local';
+        ft_databrowser(cfg, comp);
+
+        fprintf('*** SUBJECT %02d : save the identified components!!! ***\n', isub);
+
+        % write down and save
+        badcomps = [];
+        badcomps_reasons = {};
+        badcount = 0;
+        while 1
+            newcomp = input('component: ');
+            if newcomp == 0
+                break
+            else
+                badcount = badcount + 1;
+            end
+            badcomps(badcount) = newcomp;
+            newreason = input('reason: ','s');
+            badcomps_reasons{badcount} = newreason;
+        end
+
+        assert(numel(badcomps) == numel(badcomps_reasons));
+
+        fn2save = sprintf('%s%sica_badcomps_sub%03d_run%02d',indir,filesep,isub,irun);
+        save(fn2save, 'badcomps', 'badcomps_reasons');
+        clear comp
+
+        close all
+
+    end
+
+end
