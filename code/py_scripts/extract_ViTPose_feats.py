bf97fc2dfd5e12f2a5977ed113720cc7ec06e1d1
TizianoCausin01 <causintiziano@gmail.com>
2025-07-01 18:53:46 -0400

    handled exception in ViTPose extraction

--- code/py_scripts/extract_ViTPose_feats.py
+++ code/py_scripts/extract_ViTPose_feats.py
@@ -7,6 +7,7 @@ from PIL import Image
 import numpy as np
 import cv2
 import h5py
+import sys
 from scipy.linalg import inv
 from numpy.linalg import inv
 from datetime import datetime
@@ -44,14 +45,15 @@ image_processor = AutoProcessor.from_pretrained("/leonardo_work/Sis25_piasini/tc
 model = VitPoseForPoseEstimation.from_pretrained("/leonardo_work/Sis25_piasini/tcausin/Project1917/huggingface_models/vitpose-base-simple", local_files_only=True)#downloads ViTPose
 
 # 3 - read frame and preprocess it
-runs = [1,2,3]
+arg = sys.argv[1]              # "1 2 3"
+runs = list(map(int, arg.split()))
 for irun in runs: 
     path2vid = f"/leonardo_scratch/fast/Sis25_piasini/tcausin/Project1917/stimuli/Project1917_movie_part{irun}_24Hz.mp4"
     print(datetime.now().strftime("%H:%M:%S")," - irun:", irun, flush=True)
     count = 0
     reader = cv2.VideoCapture(path2vid)
+    reader.set(cv2.CAP_PROP_POS_FRAMES, 2947)
     while True:
-        reader.set(cv2.CAP_PROP_POS_FRAMES, 2947)
         ret, frame = reader.read()
         count += 1
         print(datetime.now().strftime("%H:%M:%S")," - frame", count, flush=True)
@@ -75,25 +77,32 @@ for irun in runs:
         )[0] # selects the first element in the list bc only one img
         
         person_boxes = result["boxes"][result["labels"] == 0] # index only the boxes associated with label 0 (person) in COCO class labels
-        print("person_boxes", person_boxes, flush=True)
-        score_boxes = result["scores"][result["labels"] == 0]
-        score_boxes = score_boxes.cpu().numpy()
-        # score_boxes_store = fill_in_res(person_boxes, "scores", (1, 1), 5) 
-        # feats["score_boxes"].append(score_boxes_store)
-        # converts boxes from VOC format: (x1, y1, x2, y2) to COCO format: N pers detected x 4 -> 4 cols are => (x, y, width, height)
-        person_boxes[:, 2] = person_boxes[:, 2] - person_boxes[:, 0] 
-        person_boxes[:, 3] = person_boxes[:, 3] - person_boxes[:, 1] 
-        
-    # 6 - preprocess for kpt detection
-        inputs = image_processor([frame_rgb], boxes=[person_boxes], return_tensors="pt").to(device) # processes the original image using the bounding boxes -> ViTPose expects tightly cropped pics
-        # inputs is a dict like type with "pixels_value" as only entry. It is a tensor [Batch, Channels, Height, Width] -> Batch is the number of people detected
-        with torch.no_grad():
-            outputs = model(**inputs) # runs ViTPose
-        pose_results = image_processor.post_process_pose_estimation(outputs, boxes=[person_boxes])[0]
-        kpts_store = fill_in_res(pose_results, "keypoints", (17,2), 5)
-        kpts_scores_store = fill_in_res(pose_results, "scores", (17), 5)
-        person_boxes_store = fill_in_res(pose_results, "bbox", (4), 5) 
-        score_boxes_store = fill_in_res(result, "scores", (1), 5, box=1)
+        if person_boxes.numel() == 0: # predef dimensionalities, sorry for hardcoding
+            person_boxes_store = np.full((20,), np.nan)
+            score_boxes_store = np.full((5,), np.nan)
+            kpts_store = np.full((170,), np.nan)
+            kpts_scores_store = np.full((85,), np.nan)
+            datetime.now().strftime("%H:%M:%S")," skipping frame", count, "because people weren't detected" flush=True)
+        else:
+            score_boxes = result["scores"][result["labels"] == 0]
+            score_boxes = score_boxes.cpu().numpy()
+            # score_boxes_store = fill_in_res(person_boxes, "scores", (1, 1), 5) 
+            # feats["score_boxes"].append(score_boxes_store)
+            # converts boxes from VOC format: (x1, y1, x2, y2) to COCO format: N pers detected x 4 -> 4 cols are => (x, y, width, height)
+            person_boxes[:, 2] = person_boxes[:, 2] - person_boxes[:, 0] 
+            person_boxes[:, 3] = person_boxes[:, 3] - person_boxes[:, 1] 
+            
+        # 6 - preprocess for kpt detection
+            inputs = image_processor([frame_rgb], boxes=[person_boxes], return_tensors="pt").to(device) # processes the original image using the bounding boxes -> ViTPose expects tightly cropped pics
+            # inputs is a dict like type with "pixels_value" as only entry. It is a tensor [Batch, Channels, Height, Width] -> Batch is the number of people detected
+            with torch.no_grad():
+                outputs = model(**inputs) # runs ViTPose
+            pose_results = image_processor.post_process_pose_estimation(outputs, boxes=[person_boxes])[0]
+            kpts_store = fill_in_res(pose_results, "keypoints", (17,2), 5)
+            kpts_scores_store = fill_in_res(pose_results, "scores", (17), 5)
+            person_boxes_store = fill_in_res(pose_results, "bbox", (4), 5) 
+            score_boxes_store = fill_in_res(result, "scores", (1), 5, box=1)
+        # end if person_boxes.numel() == 0:
         feats["boxes"].append(person_boxes_store) # FIXME it's a list of dicts
         feats["kpts"].append(kpts_store)
         feats["score_kpts"].append(kpts_scores_store)
